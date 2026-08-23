b3296c014ce6ad4bd49712ebca52e4615e114bbd
Alessandro Aparecido Da Silva Domingues <alessandrosilvaicas.enf@gmail.com>
2026-08-23 03:24:41 -0300

    feat(settings): wire native identity settings and communication audit

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ActionRouterController;
 use App\Http\Controllers\AutoatendimentoController;
+use App\Http\Controllers\CommunicationLogController;
 use App\Http\Controllers\CorporateAuthController;
 use App\Http\Controllers\DashboardRouterController;
+use App\Http\Controllers\EnterpriseSettingsController;
 use App\Http\Controllers\IdentityDirectoryController;
 
 Route::get('/', function () {
@@ -12,10 +15,19 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', [DashboardRouterController::class, 'index']);
-Route::post('/acao', [AdminController::class, 'acaoRapida']);
+Route::post('/acao', [ActionRouterController::class, 'handle']);
 Route::get('/login', [CorporateAuthController::class, 'login']);
 Route::post('/login', [CorporateAuthController::class, 'login']);
 Route::get('/logout', [CorporateAuthController::class, 'logout']);
+
+Route::prefix('enterprise-settings')->group(function () {
+    Route::get('/identity', [EnterpriseSettingsController::class, 'identity']);
+    Route::post('/identity', [EnterpriseSettingsController::class, 'saveIdentity']);
+    Route::post('/identity/test', [EnterpriseSettingsController::class, 'testIdentity']);
+    Route::post('/identity/sync', [EnterpriseSettingsController::class, 'syncIdentity']);
+});
+
+Route::get('/communication-logs/emails', [CommunicationLogController::class, 'emails']);
 
 Route::prefix('identity-directory')->group(function () {
     Route::get('/', [IdentityDirectoryController::class, 'index']);
